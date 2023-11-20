62cda04b25718a8c4a850d25447ca134f0ba0302
QasimKhan5x <straightouttadetroit.qk@gmail.com>
2023-11-20 11:08:55 +0100

    initial add files

---
+++ create_sf_data.py
@@ -0,0 +1,41 @@
+import json
+import random
+
+# total number of objects is 1984049
+scale_factors = [1000, 496762, 992524, 1488286, 1984049]
+random.seed(0)
+
+
+def create_sf_data(scale_factor):
+    num_objects = scale_factors[scale_factor - 1]
+    total_objects = 1984049  # total number of objects in twitter.json
+
+    # Generate a set of unique random indices to sample
+    indices_to_sample = set(random.sample(range(total_objects), num_objects))
+
+    with open("twitter.json", "r", encoding="utf-8") as file, open(
+        f"./data/twitter_sf_{scale_factor}.json", "w", encoding="utf-8"
+    ) as outfile:
+
+        for i, line in enumerate(file):
+            if i in indices_to_sample:
+                try:
+                    # Write the object to the file
+                    outfile.write(line.strip())
+                    outfile.write("\n")  # New line after each object
+
+                except json.JSONDecodeError:
+                    print(f"Error decoding JSON on line {i}")
+
+                # Remove the index from the set for efficiency
+                indices_to_sample.remove(i)
+
+                # Break if we have sampled enough
+                if not indices_to_sample:
+                    break
+
+# Example usage
+create_sf_data(4)
+
+# use this to load and measure execution time
+# Measure-Command { mongoimport --db advdb_project --collection sf5 --file data/twitter.json }
---
+++ queries/oltp/create1.py
@@ -0,0 +1,78 @@
+import random
+import time
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+# Create a MongoDB client (assuming MongoDB is running on the default host and port)
+client = MongoClient('localhost', 27017)
+
+# Use a specific database and collection
+db = client['advdb_project']
+collection = db['sf1']
+
+# Define a set of random hashtags
+hashtags = ["#Tech", "#News", "#Sports", "#Entertainment", "#Travel"]
+
+# Random data generators for various fields
+def random_user():
+    return {
+        "id": random.randint(10000, 99999),
+        "id_str": str(random.randint(10000, 99999)),
+        "name": "User" + str(random.randint(1, 100)),
+        "screen_name": "user" + str(random.randint(1, 100)),
+        "location": random.choice(["New York", "London", "Tokyo", None]),
+        "description": random.choice(["Love tech and music", "Travel enthusiast", "Sports fan", None]),
+        "followers_count": random.randint(0, 1000),
+        "friends_count": random.randint(0, 500),
+        "listed_count": random.randint(0, 100),
+        "created_at": time.strftime("%a %b %d %H:%M:%S +0000 %Y"),
+        "favourites_count": random.randint(0, 200),
+        "utc_offset": random.choice([None, 3600, -3600]),
+        "time_zone": random.choice(["EST", "PST", "GMT", None]),
+        "geo_enabled": random.choice([True, False]),
+        "verified": random.choice([True, False]),
+        "statuses_count": random.randint(100, 10000)
+    }
+
+def random_entities():
+    return {
+        "hashtags": [{"text": random.choice(["Tech", "News", "Sports", "Entertainment", "Travel"])} for _ in range(random.randint(0, 3))],
+        "urls": [],
+        "user_mentions": [{"screen_name": "user" + str(random.randint(1, 100)),
+                           "name": "User" + str(random.randint(1, 100)),
+                           "id": random.randint(10000, 99999),
+                           "id_str": str(random.randint(10000, 99999))} for _ in range(random.randint(0, 2))]
+    }
+
+def generate_data(n):
+    data = []
+    for _ in range(n):
+        obj = {
+            "_id": ObjectId(),
+            "text": "Sample tweet " + random.choice(["#Tech", "#News", "#Sports", "#Entertainment", "#Travel"]),
+            "created_at": time.strftime("%a %b %d %H:%M:%S +0000 %Y"),
+            "user": random_user(),
+            "entities": random_entities(),
+            "in_reply_to_status_id_str": None if random.choice([True, False]) else str(random.randint(10000, 99999)),
+            "coordinates": None,
+            "place": None,
+            "retweet_count": random.randint(0, 100),
+            "favorited": random.choice([True, False]),
+            "retweeted": random.choice([True, False])
+        }
+        data.append(obj)
+    return data
+
+# How many total documents?
+N = collection.count_documents({})
+# Generate data for a quarter of them
+sample_documents = generate_data(N // 4)
+
+# Insert the documents into the MongoDB collection and measure the execution time
+start_time = time.time()
+result = collection.insert_many(sample_documents)
+end_time = time.time()
+
+# Calculate the execution time
+execution_time = end_time - start_time
+print("Execution time for batch insert:", execution_time, "seconds")
